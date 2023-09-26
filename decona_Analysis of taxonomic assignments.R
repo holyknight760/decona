c8ef284a84f0b81c641293e12588294dd80b6d12
holyknight760 <84485045+holyknight760@users.noreply.github.com>
2023-09-26 16:09:45 +0900

    Update decona_Analysis of taxonomic assignments.R

--- decona_Analysis of taxonomic assignments.R
+++ decona_Analysis of taxonomic assignments.R
@@ -4,3 +4,18 @@
 #상위 분류학적 이름 검색: 생물학자들이 종 목록에 대한 상위 분류학적 이름을 얻는 것은 흔한 작업입니다. 상위 분류를 알고 있으면 종 목록의 관계를 이해하는 데 도움이 됩니다. 
 install.packages("taxize")
 library('taxize') #v0.9.100
+# 필요한 패키지를 설치하고 불러옵니다.
+if (!require(vegan)) {
+  install.packages("vegan")
+  library(vegan)
+}
+
+# 표 S6에서 데이터를 불러옵니다.
+# 이 부분은 실제 파일 경로와 파일 형식에 따라 변경해야 합니다.
+data <- read.csv("table_S6.csv")
+
+# Species accumulation curves를 계산합니다.
+sac <- specaccum(data)
+
+# 그래프를 그립니다.
+plot(sac)
